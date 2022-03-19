134ba5824efe3f2e7d2b73fc5f2f8466b3bce3ee
Harika <83266748+harikainnamuri15@users.noreply.github.com>
2022-03-19 21:33:01 +0530

    Add files via upload

---
+++ basic prgm.py
@@ -0,0 +1,10 @@
+Python 3.10.3 (tags/v3.10.3:a342a49, Mar 16 2022, 13:07:40) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print('harika\'s iphone')
+harika's iphone
+print(r"harika\n')
+      
+SyntaxError: unterminated string literal (detected at line 1)
+print(r'harika\n')
+      
+harika\n
